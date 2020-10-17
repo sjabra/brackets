344ee10360a328e379c50d748c0e45dff7e77f7f
Alex Valynets <sjabruxa@gmail.com>
2020-10-17 14:18:54 +0300

    init: start js brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openingBrackets = bracketsConfig.map((value) => value[0]).join("");
+    const closingBrackets = bracketsConfig.map((value) => value[1]).join("");
+    let bracketsStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const nextBracket = str.charAt(i);
+        
+        if (closingBrackets.includes(nextBracket)) {
+            let closingBracketIndex = closingBrackets.indexOf(nextBracket); 
+            if (bracketsStack[bracketsStack.length - 1] == openingBrackets[closingBracketIndex]) {
+                bracketsStack.pop();
+                continue;
+            } else {
+                bracketsStack.push(nextBracket);
+                continue;
+            }
+        }
+
+        if (openingBrackets.includes(nextBracket)) {
+            bracketsStack.push(nextBracket);
+        }
+    }
+
+    return bracketsStack.length == 0;
+};
